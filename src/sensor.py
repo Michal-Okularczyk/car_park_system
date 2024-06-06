0cbb151fe504d4a86c80b5f1f94772e9815c7dcd
Michal-Okularczyk <20122085@tafe.wa.edu.au>
2024-06-06 11:16:37 +0800

    Added Sensor class

--- src/sensor.py
+++ src/sensor.py
@@ -2,3 +2,13 @@
 
 class Sensor:
     pass  # Placeholder, ill add attributes and methods later
+
+class Sensor:
+    def __init__(self, id, is_active=True, car_park=None):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        status = "active" if self.is_active else "inactive"
+        return f"Sensor {self.id}: {status}"
