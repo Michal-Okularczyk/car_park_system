7b38a12182da4c3d18e3e06a15862e8eb713d4b6
Michal-Okularczyk <20122085@tafe.wa.edu.au>
2024-06-06 12:38:45 +0800

    Added Carpark Class Added Display Class Added Sensor  Class s1 Added a constructor and attributes to the car park class s2 Added constructors and attributes to the Display class s3Added a register, add_car, remove_car, and update_displays methods to the CarPark class. s4 Added methods to the car park class s5 Detect vehicle method implemented in sensor class

--- src/sensor.py
+++ src/sensor.py
@@ -1,19 +1,31 @@
 # sensor.py
 
-class Sensor:
-    pass  # Placeholder, ill add attributes and methods later
+from car_park import CarPark
+from abc import ABC, abstractmethod
+import random
 
-class Sensor:
-    def __init__(self, id, is_active=False, car_park=None):
-        self.id = id
-        self.is_active = is_active
-        self.car_park = car_park
+class Sensor(ABC):
+    @abstractmethod
+    def update_car_park(self, plate):
+        pass
 
-    def __str__(self):
-        return f"Sensor {self.id}: {'Active' if self.is_active else 'Inactive'}"
+    def _scan_plate(self):
+        return 'FAKE-' + format(random.randint(0, 999), "03d")
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
 
 class EntrySensor(Sensor):
-    pass
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
 
 class ExitSensor(Sensor):
-    pass
+    def update_car_park(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
+
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
+
