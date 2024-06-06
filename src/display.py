9cc29038a6f2a0dd1a0d48716a3c0e01f5c3ddc5
Michal-Okularczyk <20122085@tafe.wa.edu.au>
2024-06-06 11:27:40 +0800

    Added Display class

--- src/display.py
+++ src/display.py
@@ -2,3 +2,14 @@
 
 class Display:
     pass  # Placeholder, ill add attributes and methods later
+# display.py
+
+class Display:
+    def __init__(self, id, message="", is_on=False, car_park=None):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"Display {self.id}: {self.message}"
