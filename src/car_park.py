3392c2d9930d07483044ac844d1de09b9141369e
Michal-Okularczyk <20122085@tafe.wa.edu.au>
2024-06-06 12:01:10 +0800

    Added Display class s3 "Added a register, add_car, remove_car, and update_displays methods to the CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -3,8 +3,11 @@
 class CarPark:
     pass  # Placeholder, ill add attributes and methods later
 
+from sensor import Sensor
+from display import Display
+
 class CarPark:
-    def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
+    def __init__(self, location="Unknown", capacity=0, plates=None, sensors=None, displays=None):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
@@ -13,3 +16,27 @@ class CarPark:
 
     def __str__(self):
         return f"Car park at {self.location}, with {self.capacity} bays."
+
+    def register(self, component):
+        if not isinstance(component, (Sensor, Display)):
+            raise TypeError("Object must be a Sensor or Display")
+        if isinstance(component, Sensor):
+            self.sensors.append(component)
+        elif isinstance(component, Display):
+            self.displays.append(component)
+
+    def add_car(self, plate):
+        self.plates.append(plate)
+        self.update_displays()
+
+    def remove_car(self, plate):
+        if plate in self.plates:
+            self.plates.remove(plate)
+            self.update_displays()
+
+    def update_displays(self):
+        available_bays = self.capacity - len(self.plates)
+        for display in self.displays:
+            display.update(available_bays)
+
+
